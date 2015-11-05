90d2ad8b608a8893dfdefd025eeef05acef42a46
Abhishek Lekshmanan <abhishek.lekshmanan@gmail.com>
2015-11-05 12:51:22 +0530

    adding config parser

--- ceph_mon_maps.py
+++ ceph_mon_maps.py
@@ -1,19 +1,29 @@
+import sys
+
 import boto
 import boto.s3.connection
+import ConfigParser
 
 from sh import ceph
 from datetime import datetime
+
 
 
 def get_monmap(mapfile):
     status = ceph("mon", "getmap", o=mapfile)
     return status.exit_code == 0
 
+
+
 if __name__ == "__main__":
+    configfile = sys.argv[1]
     mapfile = "map" + datetime.now().isoformat()
-    access = 'some access'  # replace with argparse & cfgparse
-    secret = 'some secret'
-    host = 's3.amazonaws.com'
+    config = ConfigParser.ConfigParser()
+    config.read(configfile)
+    access = config.get('credentials','access')
+    secret = config.get('credentials','secret')
+    host = config.get('credentials','host')
+
     conn = boto.connect_s3(
             aws_access_key_id=access,
             aws_secret_access_key=secret,
